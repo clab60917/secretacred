4b801b1d7d03c3d98cb0b221cf56786a32c66303
Clément <clab60917@eleve.isep.fr>
2024-06-19 10:50:27 +0200

    script 3 V1.0

---
+++ script3.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from tqdm import tqdm, tqdm_pandas
+
+# Initialiser tqdm pour pandas
+tqdm_pandas(tqdm())
+
+def read_excel_with_progress(filename):
+    # Charger le DataFrame avec une barre de progression
+    xl = pd.ExcelFile(filename)
+    nrows = xl.book.sheet_by_index(0).nrows
+    return pd.read_excel(xl, engine='openpyxl', iterator=True, chunksize=1000).progress_apply(lambda x: x)
+
+# Lire les fichiers Excel avec barres de progression
+people = read_excel_with_progress('people.xlsx')
+custom = read_excel_with_progress('custom.xlsx')
+departements_c3 = pd.read_excel('departements.xlsx', header=None)
+
+# Créer des copies des DataFrames pour les manipulations
+people_copy = people.copy()
+custom_copy = custom.copy()
+
+# Renommer les colonnes dans 'custom_copy' pour correspondre à celles de 'people_copy'
+custom_copy.rename(columns={'GGI': 'IGG', 'Email': 'GROUP_MAIL', 'Department': 'LIB_SERVICE'}, inplace=True)
+
+# Fusionner people_copy avec custom_copy pour compléter les informations manquantes
+merged_data = pd.merge(people_copy, custom_copy[['IGG', 'GROUP_MAIL', 'LIB_SERVICE']], on='IGG', how='left', suffixes=('', '_custom'))
+
+# Utiliser fillna pour combler les informations manquantes
+merged_data['GROUP_MAIL'] = merged_data['GROUP_MAIL'].fillna(merged_data['GROUP_MAIL_custom'])
+merged_data['LIB_SERVICE'] = merged_data['LIB_SERVICE'].fillna(merged_data['LIB_SERVICE_custom'])
+
+# Supprimer les colonnes inutiles après la fusion
+merged_data.drop(columns=['GROUP_MAIL_custom', 'LIB_SERVICE_custom'], inplace=True)
+
+# Filtrer les départements C3 avec une barre de progression
+c3_departments = set(departements_c3[0])
+filtered_data = merged_data[merged_data['LIB_SERVICE'].isin(c3_departments)].progress_apply(lambda x: x)
+
+# Assurer que la colonne LIB_SERVICE est toujours remplie
+filtered_data['LIB_SERVICE'] = filtered_data['LIB_SERVICE'].fillna(method='bfill')
+
+# Sélectionner les colonnes nécessaires pour le fichier final
+final_data = filtered_data[['IGG', 'GROUP_MAIL', 'LIB_SERVICE']]
+
+# Sauvegarder le fichier final
+final_data.to_excel('C3_accredited_users.xlsx', index=False)
+
+print("Le fichier 'C3_accredited_users.xlsx' a été créé avec succès.")
