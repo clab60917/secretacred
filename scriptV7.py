76e359442acc5a39b0f4366b3d6d0882d7ce5e7b
Clément <clab60917@eleve.isep.fr>
2024-06-26 10:26:43 +0200

    scriptv7 V1.2

--- scriptV7.py
+++ scriptV7.py
@@ -27,10 +27,15 @@ print("Initialisation du script...")
 # Lire les fichiers Excel avec barres de progression
 print("\n---------------\nLecture des fichiers Excel...\n---------------")
 people = read_excel_with_progress('people.xlsx', header=0)
+print(f"'people.xlsx' est de type {type(people)}")
 custom = read_excel_with_progress('custom.xlsx', header=0)
+print(f"'custom.xlsx' est de type {type(custom)}")
 departements_c3 = read_excel_with_progress('departements.xlsx', sheet_name='LIST C3 DPT ONLY INTERNALS', header=None)
+print(f"'departements.xlsx' (LIST C3 DPT ONLY INTERNALS) est de type {type(departements_c3)}")
 nominative_users = read_excel_with_progress('departements.xlsx', sheet_name='NOMINATIVE USERS + ORIGINE', header=0)
+print(f"'departements.xlsx' (NOMINATIVE USERS + ORIGINE) est de type {type(nominative_users)}")
 elr_habilite = read_excel_with_progress('departements.xlsx', sheet_name='LIST OF ELR', header=0)
+print(f"'departements.xlsx' (LIST OF ELR) est de type {type(elr_habilite)}")
 
 # Afficher les noms des colonnes pour vérification
 print("\n---------------\nVérification des colonnes dans les fichiers Excel...\n---------------")
