c5f3e745109466a33d7853660cea2c32429e10f4
Clément <clab60917@eleve.isep.fr>
2024-08-02 13:57:36 +0200

    V6 des tests

--- tests_run.py
+++ tests_run.py
@@ -6,19 +6,17 @@ init(autoreset=True)
 
 # Chemins des fichiers
 c3_output_file = 'C3_accredited_users.xlsx'
-colleague_output_file = 'colleague_output.xlsx'
 jde_file = 'JDE.xlsx'
-comparison_file = 'comparison_data.xlsx'
+jerome_clement_file = 'comparison_data.xlsx'  # Le fichier contenant les onglets 'jerome' et 'clement'
 
 # Charger les fichiers Excel nécessaires
 c3_data = pd.read_excel(c3_output_file)
-colleague_data = pd.read_excel(colleague_output_file, header=None, names=['GROUP_MAIL'])
 jde_data = pd.read_excel(jde_file)
-comparison_data = pd.read_excel(comparison_file, sheet_name=None)
+jerome_clement_data = pd.read_excel(jerome_clement_file, sheet_name=None)
 
 # Extraire les onglets 'jerome' et 'clement'
-jerome_data = comparison_data['jerome']
-clement_data = comparison_data['clement']
+jerome_data = jerome_clement_data['jerome']
+clement_data = jerome_clement_data['clement']
 
 # Tests
 def test_nominative_users():
@@ -35,46 +33,8 @@ def test_nominative_users():
         for mail in missing_mails:
             print(Fore.RED + mail)
 
-def test_excluded_users():
-    print(Fore.BLUE + "\n--- Test des utilisateurs à exclure ---")
-    excluded_emails = set(comparison_data['DPTS-USER TO BE EXCLUDED']['Mail'].dropna())
-    
-    c3_emails = set(c3_data['GROUP_MAIL'].dropna())
-
-    included_excluded_mails = c3_emails & excluded_emails
-
-    if not included_excluded_mails:
-        print(Fore.GREEN + "[SUCCÈS] Aucun utilisateur exclu ne figure dans la liste C3.")
-    else:
-        print(Fore.RED + "[ÉCHEC] Les utilisateurs suivants, qui devaient être exclus, sont toujours présents :")
-        for mail in included_excluded_mails:
-            print(Fore.RED + mail)
-
-def test_comparison_with_colleague():
-    print(Fore.BLUE + "\n--- Test de comparaison avec la liste du collègue ---")
-    your_emails = set(c3_data['GROUP_MAIL'].dropna())
-    colleague_emails = set(colleague_data['GROUP_MAIL'].dropna())
-
-    emails_in_your_not_in_colleague = your_emails - colleague_emails
-    emails_in_colleague_not_in_your = colleague_emails - your_emails
-    common_emails = your_emails & colleague_emails
-
-    if not emails_in_your_not_in_colleague:
-        print(Fore.GREEN + "[SUCCÈS] Tous les utilisateurs de votre liste se trouvent également dans celle du collègue.")
-    else:
-        print(Fore.RED + "[ÉCHEC] Les utilisateurs suivants sont dans votre liste mais pas dans celle du collègue :")
-        for email in emails_in_your_not_in_colleague:
-            print(Fore.RED + email)
-
-    if not emails_in_colleague_not_in_your:
-        print(Fore.GREEN + "[SUCCÈS] Tous les utilisateurs de la liste du collègue se trouvent également dans la vôtre.")
-    else:
-        print(Fore.RED + "[ÉCHEC] Les utilisateurs suivants sont dans la liste du collègue mais pas dans la vôtre :")
-        for email in emails_in_colleague_not_in_your:
-            print(Fore.RED + email)
-
-def test_comparison_with_provided_lists():
-    print(Fore.BLUE + "\n--- Test de comparaison avec les listes fournies ---")
+def test_comparison_with_jerome_clement():
+    print(Fore.BLUE + "\n--- Test de comparaison avec les listes 'jerome' et 'clement' ---")
     c3_mails_depts = c3_data[['GROUP_MAIL', 'DEPARTEMENT']].dropna()
     c3_mails_depts_set = set(tuple(x) for x in c3_mails_depts.to_numpy())
 
@@ -102,9 +62,7 @@ def test_comparison_with_provided_lists():
 
 def main():
     test_nominative_users()
-    test_excluded_users()
-    test_comparison_with_colleague()
-    test_comparison_with_provided_lists()
+    test_comparison_with_jerome_clement()
 
 if __name__ == "__main__":
     main()
